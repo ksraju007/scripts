541e82768dbe5f99b59909aee9325da6c9f4d245
Rajesh Sivaraman @rajesh_says <ksraju007@gmail.com>
2015-09-02 09:25:40 +0200

    Update swarm_install.sh

--- swarm_install.sh
+++ swarm_install.sh
@@ -10,14 +10,14 @@ sudo apt-get update
 sudo apt-get install git golang -y
 
 #prepare /opt to hold new tools
-ME=`whoami` ;  GOPATH="/opt/tools" ;  sudo mkdir -p $GOPATH ; sudo chown $ME $GOPATH
+export ME=`whoami` ;  export GOPATH="/opt/tools" ;  sudo mkdir -p $GOPATH ; sudo chown $ME $GOPATH ;
 
 #Install godep , this should install godep in $GOPATH/bin/
 cd $GOPATH ; go get github.com/tools/godep
 
 #get latest release of swarm
-mkdir -p $GOPATH/src/github.com/docker ; cd $GOPATH/src/github.com/docker ; 
-git clone -b release https://github.com/docker/swarm.git && cd swarm ;
+mkdir -p $GOPATH/src/github.com/docker ; cd $GOPATH/src/github.com/docker ;  rm -rf swarm ;
+git clone -b release https://github.com/docker/swarm.git ; cd swarm ;
 $GOPATH/bin/godep go install . 
 
 #test version
