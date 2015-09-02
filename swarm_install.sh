2663c27658c0519916d519473058e30bd6dc419d
Rajesh Sivaraman @rajesh_says <ksraju007@gmail.com>
2015-09-02 09:44:29 +0200

    Update swarm_install.sh

--- swarm_install.sh
+++ swarm_install.sh
@@ -5,13 +5,12 @@ function msg {
   echo -e "\e[1;32m $1 \e[0m" ;
 }
 
-
 #Prepare
-msg Updating apt and installing pre-requisites..
+msg "Updating apt and installing pre-requisites.."
 sudo apt-get -qq update ; sudo apt-get install python-software-properties -y ;
 
 #first, install git and golang
-msg Installing git and golang latest versions..
+msg "Installing git and golang latest versions.."
 sudo add-apt-repository ppa:evarlast/golang1.4  -y
 sudo apt-get -qq update
 sudo apt-get install git golang -y
@@ -20,18 +19,18 @@ sudo apt-get install git golang -y
 export ME=`whoami` ;  export GOPATH="/opt/tools" ;  sudo mkdir -p $GOPATH ; sudo chown $ME $GOPATH ;
 
 #Install godep , this should install godep in $GOPATH/bin/
-msg Installing godep into $GOPATH/bin
+msg "Installing godep into $GOPATH/bin"
 cd $GOPATH ; go get github.com/tools/godep
 
 #get latest release of swarm
-msg Downloading latest stable release of swarm..
+msg "Downloading latest stable release of swarm.."
 mkdir -p $GOPATH/src/github.com/docker ; cd $GOPATH/src/github.com/docker ;  rm -rf swarm ;
 git clone -b release https://github.com/docker/swarm.git ; cd swarm ;
-msg Installing Swarm..
+msg "Installing Swarm.."
 $GOPATH/bin/godep go install . 
 
 #test version
-msg Testing Swarm version..
+msg "Testing Swarm version.."
 $GOPATH/bin/swarm --version
 
 if [ $? -eq 0  ] ; then
