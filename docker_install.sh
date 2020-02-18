6a3f9694c78d4f327e6de433ed888236beedbb71
Rajesh Sivaraman @rajesh_says <ksraju007@gmail.com>
2020-02-18 10:43:45 +0100

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 # This script installs latest version of docker in ubuntu
-# TODO : Add CentOS Support
+apt-get update
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+ 
+ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+ add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+ apt-get update  
+ apt-get install docker-ce docker-ce-cli containerd.io openjdk-8-jdk
+ curl -L "https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+ chmod +x /usr/local/bin/docker-compose
+ 
+ docker -v 
+ docker-compose -v
+ 
 
-echo Installing/Updating Curl
-sudo apt-get update
-sudo apt-get install curl
-
-echo Installing Docker
-curl -sSL https://get.docker.com/ | sh
-
-echo Testing Docker
-sudo docker run hello-world
-
-echo "If you want to enable your current user to use docker, run the below command."
-echo "sudo usermod -aG docker <username>"
-echo "After you run the command, please logout and login"
