6f39918e886716328435710f87564662c45e73ab
Rajesh Sivaraman @rajesh_says <ksraju007@gmail.com>
2020-02-26 18:02:34 +0100

    Update dotnet.sh

--- dotnet.sh
+++ dotnet.sh
@@ -6,4 +6,14 @@ apt-get update
 apt-get install -y apt-transport-https 
 apt-get update
 apt-get install -y dotnet-sdk-3.1  dotnet-sdk-2.1
+useradd builder
+chsh -s /bin/bash builder
 dotnet tool install --global dotnet-sonarscanner --version 4.7.1
+
+echo creating builder user and creating ssh key
+useradd -m builder
+chsh -s /bin/bash builder
+su -c "ssh-keygen -t rsa -N '' -f /home/builder/.ssh/id_rsa"  -l builder
+su -c "cp /home/builder/.ssh/id_rsa.pub /home/builder/.ssh/authorized_keys" -l builder
+cat /home/builder/.ssh/id_rsa
+echo "copy the above key to jenkins as a private key credential to configure hosts."
