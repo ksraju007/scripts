eefa5e8c8a24f3633f9c3a29573f93139d37bdb5
Rajesh Sivaraman @rajesh_says <ksraju007@gmail.com>
2020-02-26 23:20:30 +0100

    Update dotnet.sh

--- dotnet.sh
+++ dotnet.sh
@@ -8,11 +8,11 @@ apt-get update
 apt-get install -y dotnet-sdk-3.1  dotnet-sdk-2.1
 useradd builder
 chsh -s /bin/bash builder
-dotnet tool install --global dotnet-sonarscanner --version 4.7.1
 
 echo creating builder user and creating ssh key
 useradd -m builder
 chsh -s /bin/bash builder
+su -c "dotnet tool install --global dotnet-sonarscanner --version 4.7.1" -l builder
 su -c "ssh-keygen -t rsa -N '' -f /home/builder/.ssh/id_rsa"  -l builder
 su -c "cp /home/builder/.ssh/id_rsa.pub /home/builder/.ssh/authorized_keys" -l builder
 cat /home/builder/.ssh/id_rsa
