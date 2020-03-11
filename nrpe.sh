f88fb2bd2c21ff1f02feb81562942f145a1036dd
Rajesh Sivaraman @rajesh_says <ksraju007@gmail.com>
2020-03-11 18:08:37 +0100

    Update nrpe.sh
    automated installation

--- nrpe.sh
+++ nrpe.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+
+if [ $# -lt 1  ] ; then 
+  echo "Server IP should be provided as argument"
+  exit 1
+fi
+
 cd /tmp 
 curl -O https://assets.nagios.com/downloads/nagiosxi/agents/linux-nrpe-agent.tar.gz
 tar xzf linux-nrpe-agent.tar.gz
 cd linux-nrpe-agent
-./fullinstall
+./fullinstall -n -i $1
+
