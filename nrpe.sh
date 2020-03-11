b60db7b93f6edb023ffebbdb07771c6e432e27e9
Rajesh Sivaraman @rajesh_says <ksraju007@gmail.com>
2020-03-11 18:01:23 +0100

    Update nrpe.sh

--- nrpe.sh
+++ nrpe.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /tmp 
-wget https://assets.nagios.com/downloads/nagiosxi/agents/linux-nrpe-agent.tar.gz
+curl -O https://assets.nagios.com/downloads/nagiosxi/agents/linux-nrpe-agent.tar.gz
 tar xzf linux-nrpe-agent.tar.gz
 cd linux-nrpe-agent
 ./fullinstall
